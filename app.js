57bf7bb6369a5c86d8f85a69a15a209eca7c71b1
Paul Reichelt <paul.reichelt@outlook.com>
2017-04-25 23:44:49 +0200

    a first example

--- app.js
+++ app.js
@@ -1,1 +1,54 @@
-console.log("Hello NodeJS!")
+const grpc = require('grpc');
+
+// load proto
+const protos = grpc.load('./protos/graphp/graphresponse.proto');
+const { Dgraph, Request, NQuad, Mutation, Value } = protos.graphp;
+
+// create new client instance
+const dgraph = new Dgraph('localhost:8080', grpc.credentials.createInsecure());
+
+// set mutation
+let setReq = new Request();
+setReq.mutation = new Mutation();
+
+// a string nquad
+let nqName = new NQuad();
+nqName.subject = 'paul';
+nqName.predicate = 'name';
+nqName.objectValue = new Value().set('str_val', 'Paul Reichelt');
+nqName.objectType = 10;
+
+// a float nquad
+let nqAge = new NQuad();
+nqAge.subject = 'paul';
+nqAge.predicate = 'size';
+nqAge.objectValue = new Value().set('double_val', 1.85);
+nqAge.objectType = 3;
+
+// add all the mutations
+setReq.mutation.set.push(nqName);
+setReq.mutation.set.push(nqAge);
+
+// run set and if successful then run query
+dgraph.run(setReq, (err, res) => {
+  if(err){
+    console.log(err);
+    return;
+  }
+
+  // query data
+  let queryReq = new Request();
+  queryReq.query = "query { me(id:paul) { name, size } }";
+
+  dgraph.run(queryReq, (err, res) => {
+    // get me
+    let me = res.n[0].children[0];
+
+    // list all properties
+    me.properties.forEach(function (prop) {
+      console.log(prop.prop + ": " + prop.value[prop.value.val]);
+    });
+  });
+
+});
+
--- update-protos.sh
+++ update-protos.sh
@@ -1,24 +1,28 @@
 #!/usr/bin/env bash
-# Download the protos directly from dgraph repository
+# Download the protos directly from dgraph repository. We replace the absolute imports with
+# relative ones.
 
-rm -rf ./github.com/
+rm -rf ./protos
 
-mkdir -p ./github.com/dgraph-io/dgraph/protos/facetsp
-curl https://raw.githubusercontent.com/dgraph-io/dgraph/release/v0.7.5/protos/facetsp/facets.proto \
-> ./github.com/dgraph-io/dgraph/protos/facetsp/facets.proto
+IMPORT_GLOBAL="github.com/dgraph-io/dgraph/protos/"
+IMPORT_LOCAL="../"
 
-mkdir -p ./github.com/dgraph-io/dgraph/protos/graphp
+mkdir -p ./protos/facetsp
+curl -s https://raw.githubusercontent.com/dgraph-io/dgraph/release/v0.7.5/protos/facetsp/facets.proto \
+| sed "s,$IMPORT_GLOBAL,$IMPORT_LOCAL,g" > ./protos/facetsp/facets.proto
+
+mkdir -p ./protos/graphp
 curl https://raw.githubusercontent.com/dgraph-io/dgraph/release/v0.7.5/protos/graphp/graphresponse.proto \
-> ./github.com/dgraph-io/dgraph/protos/graphp/graphresponse.proto
+| sed "s,$IMPORT_GLOBAL,$IMPORT_LOCAL,g" > ./protos/graphp/graphresponse.proto
 
-mkdir -p ./github.com/dgraph-io/dgraph/protos/taskp
+mkdir -p ./protos/taskp
 curl https://raw.githubusercontent.com/dgraph-io/dgraph/release/v0.7.5/protos/taskp/task.proto \
-> ./github.com/dgraph-io/dgraph/protos/taskp/task.proto
+| sed "s,$IMPORT_GLOBAL,$IMPORT_LOCAL,g" > ./protos/taskp/task.proto
 
-mkdir -p ./github.com/dgraph-io/dgraph/protos/typesp
+mkdir -p ./protos/typesp
 curl https://raw.githubusercontent.com/dgraph-io/dgraph/release/v0.7.5/protos/typesp/types.proto \
-> ./github.com/dgraph-io/dgraph/protos/typesp/types.proto
+| sed "s,$IMPORT_GLOBAL,$IMPORT_LOCAL,g" > ./protos/typesp/types.proto
 
-mkdir -p ./github.com/dgraph-io/dgraph/protos/workerp
+mkdir -p ./protos/workerp
 curl https://raw.githubusercontent.com/dgraph-io/dgraph/release/v0.7.5/protos/workerp/payload.proto \
-> ./github.com/dgraph-io/dgraph/protos/workerp/payload.proto
+| sed "s,$IMPORT_GLOBAL,$IMPORT_LOCAL,g" > ./protos/workerp/payload.proto
